92dac9a202ee81e736e0d2d1b04eab4d310dacd1
Vinícius de Alencar Freire <alencarfreire@gmail.com>
2024-05-21 14:59:21 -0300

    link wpp flu

--- src/ui/components/WhatsAppFlutuante.tsx
+++ src/ui/components/WhatsAppFlutuante.tsx
@@ -1,8 +1,8 @@
 export default function WhatsAppFlutuante() {
   return (
     <a
-      href="https://whatsapp.portoseguroroteiros.com.br/run/sitepsr"
-      className="bg-green-500 p-3 rounded-full fixed bottom-8 right-8 lg:hidden z-10 md:hidden"
+      href="https://api.whatsapp.com/send?phone=5573998344769&text=Ol%C3%A1%2C%20vim%20pelo%20site%2C%20gostaria%20de%20saber%20mais%20sobre%20o%20transfer..."
+      className="bg-green-500 p-3 rounded-full fixed bottom-8 right-8 lg:hidden z-10"
       target="_blank"
     >
       <svg
